b3ec4633beca390cf273370961dfe64feafc156e
Peerin <1461200635@qq.com>
2024-03-16 10:50:43 +0800

    add buuctf-Universe_final_answer

---
+++ buuctf/Universe_final_answer/exp.py
@@ -0,0 +1,79 @@
+# (((((iVar7 * 0x5f + iVar3 * -0x14 + iVar4 * 0xc + iVar10 * 0x54 + iVar12 * -0x2d + iVar5 +
+#            iVar6 * 0x61 + iVar9 * 0x3a + iVar11 * -0x55 == 0x3145) &&
+#          (iVar8 = (int)input[9],
+#          iVar7 * -0xf + iVar3 * -0x56 + iVar4 * -0x29 + iVar10 * -0x73 + iVar12 * -0x42 +
+#          iVar5 * -0x51 + iVar6 * -0x7a + iVar9 * -0x1e + iVar11 * -0x46 + iVar8 * 0x1e == -0xd480))
+#         && (iVar12 * 0x1f + iVar7 * -0x29 + iVar3 * 0x4e + iVar4 * -0x59 + iVar10 * 0x30 +
+#             iVar5 * 0x6c + iVar6 * -0x40 + iVar9 * 0x78 + iVar11 * -0x78 + iVar8 * -0x67 == -0x282b)
+#         )
+#         && ((iVar11 * 0x30 +
+#                iVar9 * -0x77 +
+#                iVar7 * 0x4f + iVar3 * 0x55 + iVar4 * -0x6f + iVar10 * -0x1e + iVar12 * 99 +
+#                iVar5 * 0x80 + iVar6 * 0x47 + iVar8 * -0x10 == 0x5947 &&
+#               (iVar7 * -0x62 + iVar3 * 0x16 + iVar4 * -0x45 + iVar10 * 10 + iVar12 * -0x75 +
+#                iVar5 * 99 + iVar6 * -0x13 + iVar9 * 0x7a + iVar11 * 0x17 + iVar8 * 5 == -0xb80))))
+#       && ((uVar2 = uVar1,
+#           iVar11 * 100 +
+#           iVar6 * 0x5f +
+#           iVar10 * -0xb + iVar3 * 0x7c + iVar7 * -0x55 + iVar4 * -0x52 + iVar12 * -8 + iVar5 * -0x3c
+#           + iVar9 * -0x17 + iVar8 * -0x36 == -0x8ae &&
+#           ((iVar11 * -0x3f +
+#             iVar9 * 0x4d +
+#             iVar6 * 0x10 +
+#             iVar12 * 0x1a + iVar4 * 0x49 + iVar3 * 0x29 + iVar7 * -0x39 + iVar10 * -0x12 +
+#             iVar5 * -0x6f + iVar8 * -0x53 == -0x33ca &&
+#            (iVar3 * 0x50 + iVar7 * -0x55 + iVar4 * 0x3c + iVar10 * 0x55 + iVar12 * 0x5f +
+#             iVar5 * -0x79 + iVar6 * -0x68 + iVar9 * 0x42 + iVar11 * -0x30 + iVar8 * 0x51 == -0x617))
+#           ))))
+#       &&
+#      (((iVar7 * 0x12 + iVar3 * -0x1c + iVar4 * 0x5a + iVar10 * -0x65 + iVar12 * -0x53 + iVar5 * 0x75
+#        + iVar6 * 7) - iVar9) + iVar11 * -0x55 + iVar8 * 0x65 == 0x18a4)
+# iVar3 = (int)input[1];
+# iVar7 = (int)*input;
+# iVar4 = (int)input[2];
+# iVar10 = (int)input[3];
+# iVar12 = (int)input[4];
+# iVar6 = (int)input[6];
+# iVar5 = (int)input[5];
+# iVar9 = (int)input[7];
+# iVar11 = (int)input[8];
+# uVar2 = 0;
+# uVar1 = 0;
+########################################################################################################################################################################
+# (iVar7 * 0x5f + iVar3 * -0x14 + iVar4 * 0xc + iVar10 * 0x54 + iVar12 * -0x2d + iVar5 + iVar6 * 0x61 + iVar9 * 0x3a + iVar11 * -0x55 == 0x3145)
+# (iVar8 = (int)input[9],iVar7 * -0xf + iVar3 * -0x56 + iVar4 * -0x29 + iVar10 * -0x73 + iVar12 * -0x42 + iVar5 * -0x51 + iVar6 * -0x7a + iVar9 * -0x1e + iVar11 * -0x46 + iVar8 * 0x1e == -0xd480)
+# (iVar12 * 0x1f + iVar7 * -0x29 + iVar3 * 0x4e + iVar4 * -0x59 + iVar10 * 0x30 + iVar5 * 0x6c + iVar6 * -0x40 + iVar9 * 0x78 + iVar11 * -0x78 + iVar8 * -0x67 == -0x282b)
+# (iVar11 * 0x30 + iVar9 * -0x77 + iVar7 * 0x4f + iVar3 * 0x55 + iVar4 * -0x6f + iVar10 * -0x1e + iVar12 * 99 + iVar5 * 0x80 + iVar6 * 0x47 + iVar8 * -0x10 == 0x5947)
+# (iVar7 * -0x62 + iVar3 * 0x16 + iVar4 * -0x45 + iVar10 * 10 + iVar12 * -0x75 +  iVar5 * 99 + iVar6 * -0x13 + iVar9 * 0x7a + iVar11 * 0x17 + iVar8 * 5 == -0xb80)
+# (uVar2 = uVar1, iVar11 * 100 + iVar6 * 0x5f + iVar10 * -0xb + iVar3 * 0x7c + iVar7 * -0x55 + iVar4 * -0x52 + iVar12 * -8 + iVar5 * -0x3c + iVar9 * -0x17 + iVar8 * -0x36 == -0x8ae)
+# (iVar11 * -0x3f + iVar9 * 0x4d + iVar6 * 0x10 + iVar12 * 0x1a + iVar4 * 0x49 + iVar3 * 0x29 + iVar7 * -0x39 + iVar10 * -0x12 + iVar5 * -0x6f + iVar8 * -0x53 == -0x33ca)
+# (iVar3 * 0x50 + iVar7 * -0x55 + iVar4 * 0x3c + iVar10 * 0x55 + iVar12 * 0x5f + iVar5 * -0x79 + iVar6 * -0x68 + iVar9 * 0x42 + iVar11 * -0x30 + iVar8 * 0x51 == -0x617)
+# (((iVar7 * 0x12 + iVar3 * -0x1c + iVar4 * 0x5a + iVar10 * -0x65 + iVar12 * -0x53 + iVar5 * 0x75 + iVar6 * 7) - iVar9) + iVar11 * -0x55 + iVar8 * 0x65 == 0x18a4)
+import z3
+
+input_array = z3.Array("input_array", z3.IntSort(), z3.IntSort())
+iv3,iv7,iv4,iv10,iv12,iv6,iv5,iv9,iv11,uv2,uv1 = input_array[1],input_array[0],input_array[2],input_array[3],input_array[4],input_array[6],input_array[5],input_array[7],input_array[8],0,0
+
+constraints = []
+constraints.append(iv7 * 0x5f + iv3 * -0x14 + iv4 * 0xc + iv10 * 0x54 + iv12 * -0x2d + iv5 + iv6 * 0x61 + iv9 * 0x3a + iv11 * -0x55 == 0x3145)
+iv8 = input_array[9]
+constraints.append(iv7 * -0xf + iv3 * -0x56 + iv4 * -0x29 + iv10 * -0x73 + iv12 * -0x42 + iv5 * -0x51 + iv6 * -0x7a + iv9 * -0x1e + iv11 * -0x46 + iv8 * 0x1e == -0xd480)
+constraints.append(iv12 * 0x1f + iv7 * -0x29 + iv3 * 0x4e + iv4 * -0x59 + iv10 * 0x30 + iv5 * 0x6c + iv6 * -0x40 + iv9 * 0x78 + iv11 * -0x78 + iv8 * -0x67 == -0x282b)
+constraints.append(iv11 * 0x30 + iv9 * -0x77 + iv7 * 0x4f + iv3 * 0x55 + iv4 * -0x6f + iv10 * -0x1e + iv12 * 99 + iv5 * 0x80 + iv6 * 0x47 + iv8 * -0x10 == 0x5947)
+constraints.append(iv7 * -0x62 + iv3 * 0x16 + iv4 * -0x45 + iv10 * 10 + iv12 * -0x75 + iv5 * 99 + iv6 * -0x13 + iv9 * 0x7a + iv11 * 0x17 + iv8 * 5 == -0xb80)
+uv2 = uv1
+constraints.append(iv11 * 100 + iv6 * 0x5f + iv10 * -0xb + iv3 * 0x7c + iv7 * -0x55 + iv4 * -0x52 + iv12 * -8 + iv5 * -0x3c + iv9 * -0x17 + iv8 * -0x36 == -0x8ae)
+constraints.append(iv11 * -0x3f + iv9 * 0x4d + iv6 * 0x10 + iv12 * 0x1a + iv4 * 0x49 + iv3 * 0x29 + iv7 * -0x39 + iv10 * -0x12 + iv5 * -0x6f + iv8 * -0x53 == -0x33ca)
+constraints.append(iv3 * 0x50 + iv7 * -0x55 + iv4 * 0x3c + iv10 * 0x55 + iv12 * 0x5f + iv5 * -0x79 + iv6 * -0x68 + iv9 * 0x42 + iv11 * -0x30 + iv8 * 0x51 == -0x617)
+constraints.append(((iv7 * 0x12 + iv3 * -0x1c + iv4 * 0x5a + iv10 * -0x65 + iv12 * -0x53 + iv5 * 0x75 + iv6 * 7) - iv9) + iv11 * -0x55 + iv8 * 0x65 == 0x18a4)
+
+s = z3.Solver()
+s.add(constraints)
+s.check()
+part_flag = "".join([chr(s.model().eval(input_array[i]).as_long()) for i in range(10)])
+
+command_string = f"echo {part_flag} | ./UniverseFinalAnswer"
+import subprocess
+output = subprocess.check_output(["sh", "-c", command_string], text=True)
+print(output[output.index("actf"):],end='')
+
